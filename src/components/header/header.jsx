98e89ad3f78d7b81faa994498e52be5218c39435
Sergey Lebedev <salewan@gmail.com>
2019-09-19 15:45:46 +0700

    table proto

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const Header = () => {
+
+  return <Navbar color="light" light expand="md">
+    <NavbarBrand href="/">reactstrap</NavbarBrand>
+  </Navbar>
+
+};
+
+export default Header;
---
+++ src/components/students/students.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Column, Table, AutoSizer} from 'react-virtualized';
+
+import './students.css';
+
+const list = [
+  {
+    fio: 'Ivan Ivanovich Ivanov',
+    born: '01.01.1990',
+    rate: 5
+  },
+  {
+    fio: 'PetrPetrovich Petrov',
+    born: '02.02.1992',
+    rate: 5
+  },
+]
+
+
+class Students extends React.PureComponent {
+
+  render() {
+    return <AutoSizer disableHeight>
+      {({width}) =>
+        <Table
+          width={width}
+          height={300}
+          headerHeight={30}
+          rowHeight={30}
+          rowCount={list.length}
+          rowGetter={({index}) => list[index]}
+          headerClassName='headerColumn'
+          rowClassName={this._rowClassName}
+        >
+          <Column
+            label='Name'
+            dataKey='fio'
+            width={500}
+          />
+          <Column
+            width={200}
+            label='Born'
+            dataKey='born'
+          />
+          <Column
+            width={200}
+            label={"Rate"}
+            dataKey='rate'
+          />
+        </Table>
+      }
+    </AutoSizer>
+  }
+
+  _rowClassName({index}) {
+    if (index < 0) {
+      return 'headerRow';
+    } else {
+      return index % 2 === 0 ? 'evenRow' : 'oddRow';
+    }
+  }
+};
+
+export default Students;
